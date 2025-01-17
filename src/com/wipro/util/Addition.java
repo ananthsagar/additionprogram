0f5f5c59b696c985f4fa8ce5a9fdc618c836489c
ananthsagar <49123130+ananthsagar@users.noreply.github.com>
2025-01-17 23:13:36 +0000

    Update Addition.java

--- src/com/wipro/util/Addition.java
+++ src/com/wipro/util/Addition.java
@@ -2,20 +2,16 @@ package com.wipro.util;
 
 public class Addition {
     public static void main(String[] args) {
+    	// Parse the arguments as integers
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int sum = num1 + num2;
+    }
+    }
         // Check if two arguments are provided
-        if (args.length < 2) {
-            System.out.println("Please provide two numbers as arguments.");
-            return;
-        }
+        
 
-        try {
-            // Parse the arguments as integers
-            int num1 = Integer.parseInt(args[0]);
-            int num2 = Integer.parseInt(args[1]);
-            int sum = num1 + num2;
-            System.out.println("The sum is: " + sum);
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input. Please provide valid integers.");
-        }
-    }
-}
+       
+            
+            
+     
